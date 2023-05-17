8c355130a17f77770fd182190e4dba03d492b6f0
Dybreak <sangutan.james@yahoo.com>
2023-05-18 01:12:59 +0800

    Add cors policy

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(o => o.AddPolicy("MyCorsPolicy", builder =>
+{
+    builder.WithOrigins("http://localhost:4200", "https://agreeable-grass-0514f6a10.3.azurestaticapps.net")
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
